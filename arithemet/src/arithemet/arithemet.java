d1475aa85468b082b2e2cb85d44f0422bef6b669
NseaBlu <1014119073@qq.com>
2016-12-03 10:49:18 +0800

    框架

---
+++ arithemet/src/arithemet/arithemet.java
@@ -0,0 +1,10 @@
+package arithemet;
+
+public class arithemet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ arithemet/src/arithemet/pritfResult.java
@@ -0,0 +1,5 @@
+package arithemet;
+
+public class pritfResult {
+
+}
