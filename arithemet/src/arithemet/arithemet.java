eae8fbb091931f51830a4ea8282e0ade2b15e4d7
cnlkl <nowlinlei@outlook.com>
2016-12-03 21:46:22 +0800

    修改主函数

--- arithemet/src/arithemet/arithemet.java
+++ arithemet/src/arithemet/arithemet.java
@@ -27,7 +27,81 @@ public class arithemet {
 		System.out.println("请问你想做几道题呢？n=");
 		aa.n=aa.scanner.nextInt();
 	
+		//随机生成题目并计算正确结果
 		
+		for(int i=0;i<aa.n;i++)
+		{
+			aa.numStack.clear();
+			aa.signStack.clear();
+	        aa.gg.genequestion(aa.numStack, aa.signStack);
+			aa.cc.cacul(aa.numStack, aa.signStack,i,aa.result);
+		}
+		
+		System.out.println("请输入你的答案：");
+		
+		//读入用户答案
+		int fenzi1;
+		int fenmu1;
+		int length;
+		int flag = 0;
+		char c;
+		String fenzi=null;
+	    String  fenmu=null;
+	    aa.scanner.nextLine();
+		for(int i=0;i<aa.n;i++){
+			
+			System.out.println("第"+(i+1)+"题回答：");
+			answer[i]=aa.scanner.nextLine();
+			System.out.println(answer[i]);
+		}
+		
+		//将用户答案转成shu类型
+		for(int i=0;i<aa.n;i++){
+			
+		fenzi=null;
+		fenmu=null;
+		flag=0;
+		length=answer[i].length();
+	
+		for(int j=0;j<length;j++)
+		{
+			
+			
+			c=answer[i].charAt(j);
+			if(c!='/'&&flag==0)
+			{
+				fenzi=fenzi+c;
+			}
+			else if	(answer[i].charAt(j)=='/')
+			{
+				flag=1;
+			
+			}
+			else if(c!='/'&&flag==1)
+			{
+			  fenmu=fenmu+c;
+			}
+		}
+		
+		
+		
+			fenzi=fenzi.substring(4, fenzi.length());
+		    fenzi1=Integer.parseInt(fenzi);
+		    aa.result[i]=new shu();
+		    aa.result[i].setFenZi(fenzi1);
+			//System.out.println(fenzi1);
+		    if(flag==1)
+		  {
+			  fenmu=fenmu.substring(4,fenmu.length());
+			  fenmu1=Integer.parseInt(fenmu);
+			  aa.result[i].setFenMu(fenmu1);
+			//  System.out.println(fenmu1);
+		   }
+		  
+	}
+		
+		//输出结果
+		aa.pp.printt(aa.result, aa.userResult,aa.n);
 	
 	}
 		
