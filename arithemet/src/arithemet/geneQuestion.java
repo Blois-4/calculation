392203b8054017bbb94d2d0ef28604dd51a4f7e6
NseaBlu <1014119073@qq.com>
2016-12-03 11:05:27 +0800

    框架补充

--- arithemet/src/arithemet/geneQuestion.java
+++ arithemet/src/arithemet/geneQuestion.java
@@ -1,5 +1,15 @@
 package arithemet;
 
+import java.util.Stack;
+
 public class geneQuestion {
+	public geneQuestion()
+	{
+		
+	}
+	public void genequestion(Stack numStack,Stack signStack)
+	{
+		
+	}
 
 }
--- arithemet/src/arithemet/pritfResult.java
+++ arithemet/src/arithemet/pritfResult.java
@@ -1,5 +1,15 @@
 package arithemet;
 
 public class pritfResult {
+	
+	
+	public pritfResult()
+	{
+		
+	}
+	public void printt( int []result, int []userResult)
+	{
+		
+	}
 
 }
