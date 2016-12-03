23a09844574efc5eb660c44336d5277f3dfd3356
Orlando17 <1041230983@qq.com>
2016-12-03 11:53:56 +0800

    输出类函数

--- arithemet/src/arithemet/pritfResult.java
+++ arithemet/src/arithemet/pritfResult.java
@@ -7,9 +7,26 @@ public class pritfResult {
 	{
 		
 	}
-	public void printt( int []result, int []userResult)
+	public void printt( int []result, int []userResult,int n)
 	{
 		
+			int sum=0;
+			for(int i=0;i<n;i++){
+				if(result[i]==userResult[i]){
+					sum++;
+					System.out.println(i+1+".答案正确！");
+					
+				}
+				else{
+					System.out.println(i+1+".答案错误！");
+					
+				}
+					
+				
+			}
+			System.out.println("正确题数："+sum);
+			System.out.println("程序结束！");
 	}
+		
 
 }
